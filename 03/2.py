88e35f4bbd581e392d688bda765489d712ed8f75
Oleg Izgagin <37784140+oizgagin@users.noreply.github.com>
2020-12-03 15:07:17 +0300

    Add 3.2

---
+++ 03/2.py
@@ -0,0 +1,33 @@
+def slopes(lines, dx, dy):
+    trees = 0
+
+    x, y = 0, 0
+    while y < len(lines):
+        if lines[y][x] == '#':
+            trees += 1
+
+        x, y = (x + dx) % len(lines[0]), y + dy
+
+    return trees
+
+
+def solution(lines):
+    return (
+        slopes(lines, 1, 1) *
+        slopes(lines, 3, 1) *
+        slopes(lines, 5, 1) *
+        slopes(lines, 7, 1) *
+        slopes(lines, 1, 2)
+    )
+
+
+def parse(lines):
+    return lines
+
+
+def main():
+    lines = map(str.strip, open("input", "r").readlines())
+    print solution(parse(lines))
+
+
+main()
